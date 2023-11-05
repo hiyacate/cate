e49c91d957ba649b020420a43ad640543df35f38
hiyacate <hiyacate@gmail.com>
2023-11-05 20:30:20 +0800

    This is my cate

---
+++ hiya_eureka/src/bin/start.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+SERVICE_NAME=hiya_eureka.jar
+SERVICE_PORT=8761
+LOGGING_PATH=/home/hiya/log/hiya_eureka
+JVM_LOG_DIR=/home/hiya/log/hiya_eureka/jvm
+
+
+set -e
+DIR=$(cd `dirname $0`; pwd|awk -F':' '{print $1}')
+chmod +x $DIR/*.sh
+set +e
+
+PIDS=`jps -l | grep ${SERVICE_NAME} | grep -v grep | awk '{print $1}'`
+
+if [ ${PIDS} ];
+then
+ echo "ERROR: ${SERVICE_name} already started!"
+ echo "PID: ${PIDS}"
+ exit 1
+fi
+
+if [ ! -d "$JVM_LOG_DIR" ]; then
+	mkdir -p $JVM_LOG_DIR
+fi
+
+nohup java -server -Xmx1G -Xms1G \
+ -XX:+UseG1GC \
+ -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=$JVM_LOG_DIR -XX:ErrorFile=$JVM_LOG_DIR/hiya_eureka_err_pid%p.log \
+ -Dhost.name=$HOSTNAME \
+ -DAPP_HOME=$DIR \
+ -Dlogging.path=${LOGGING_PATH} \
+ -jar $DIR/${SERVICE_NAME} "$@" \
+ > /dev/null 2>&1 &
+
+
+echo -e "Starting the ${SERVICE_NAME} .\c"
+COUNT=0
+while [ ${COUNT} -lt 1 ]; do
+for((i=1;i<360;i++));
+do
+echo -e ".\c"
+sleep 1
+COUNT=`netstat -an | grep ${SERVICE_PORT} | grep LISTEN |grep -v grep | wc -l`
+if [ $i -gt 300 ]; then
+ echo ""
+ echo "ERROR: Service ${SERVICE_NAME} start [FAILURE]"
+ exit 1
+elif [ ${COUNT} -gt 0 ]; then
+ break
+fi
+done
+
+done
+
+echo "OK!"
+
+PIDS=`jps -l | grep ${SERVICE_NAME} | grep -v grep | awk '{print $1}'`
+
+echo "PID: ${PIDS}"
+
+if [ ! ${PIDS} ];
+then
+ echo "ERROR: ${SERVICE_NAME} start [FAILURE]!"
+ exit 1
+else
+ echo "${SERVICE_NAME} start success!"
+ exit 0
+fi
---
+++ hiya_gateway/demoGateway/src/bin/stop.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+SERVICE_NAME=demoGateway.jar
+SERVICE_PORT=10010
+
+PIDS=`jps | grep ${SERVICE_NAME} | grep -v grep | awk '{print $1}'`
+
+if [ -z "$PIDS" ]; then
+ echo "No ${SERVICE_NAME} to stop"
+ exit 0
+else
+ kill -s TERM $PIDS
+fi
+
+echo -e "Service ${SERVICE_NAME} stoping .\c"
+COUNT=1
+while [ ${COUNT} -gt 0 ]; do
+ for i in `seq 1 360`;
+do
+echo -e ".\c"
+sleep 1
+COUNT=`netstat -an | grep ${SERVICE_PORT} | grep LISTEN | grep -v grep | wc -l`
+if [ $i -gt 300 ]; then
+ echo "\n"
+ echo "ERROR: ${SERVICE_NAME} stop [FAILURE]!"
+ exit 1
+elif [ ${COUNT} -lt 1 ]; then
+ echo "\n"
+ echo "Service ${SERVICE_NAME} stop sucess!"
+ break
+fi
+done
+
+done
